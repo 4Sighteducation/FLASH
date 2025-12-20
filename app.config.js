58ac1fc506e7df726092ef269991feec18b65618
4Sighteducation <admin@vespa.academy>
2025-12-20 08:27:52 +0000

    Bump iOS build number to 15

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.foursighteducation.flash",
-      buildNumber: "14",
+      buildNumber: "15",
       usesAppleSignIn: true,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
